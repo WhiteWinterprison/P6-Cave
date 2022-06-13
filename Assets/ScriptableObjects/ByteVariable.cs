8f32484f7234c761994d0647eaf6c046623bebd6
liSiGN <Lilliana11200orlanda>
2022-06-14 00:26:30 +0200

    Trying out ScriptableObjects
     - created a new Scene for me to test stuff
     - created the new Folder "ScriptableObjects" for all correlated scripts/objects/prefabs

---
+++ Assets/ScriptableObjects/ByteVariable.cs
@@ -0,0 +1,13 @@
+//Lisa Fröhlich Gabra, ER, P6, "HEL: The Human Ecosystem Laboratory"
+
+//Trying to figure out how ScriptableObjects work
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ByteVariable : ScriptableObject
+{
+    public byte Value;
+}

d17be520dbd9735aa36b23d9f909185d32583f5b
liSiGN <Lilliana11200orlanda>
2022-06-17 20:59:35 +0200

    Player State Machine WiP
     - started setup for adding three more scripts onto the player prefabs (disable trackers, disable AudioListeners, enable Teleportation)
    
    only committed this so I can checkout a another branch to look at the player prefabs there

---
+++ Assets/Scripts/Li_DisableOtherTrackers.cs
@@ -0,0 +1,40 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//Lisa Fröhlich Gabra, Expanded Realities, Semester 6th//
+//Group 1: HEL                                         //
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+
+//Script: Disableing the other users cameras to avoid rendering problems
+
+
+//What it do:
+// - check if the player is inside a room
+// - searches for all other players than the users
+// - disables all the other players cameras
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Li_DisableOtherTrackers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                if (!obj.GetComponent<PhotonView>().IsMine)
+                {
+                    foreach (Camera cam in obj.GetComponentsInChildren<Camera>())
+                    {
+                        cam.enabled = false;
+                    }
+                }
+            }
+        }
+    }
+}

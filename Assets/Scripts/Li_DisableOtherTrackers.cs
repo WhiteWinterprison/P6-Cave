766f699df6faaa050079ccc1ee9025cdeb3e6724
liSiGN <Lilliana11200orlanda>
2022-06-18 13:55:28 +0200

    Player State Machine WiP
     - finished the script to disable the trackers, it does also disable the controllers now

--- Assets/Scripts/Li_DisableOtherTrackers.cs
+++ Assets/Scripts/Li_DisableOtherTrackers.cs
@@ -11,12 +11,14 @@
 // - check if the player is inside a room
 // - searches for all other players than the users
 // - disables all the other players Tracked Pose Drivers
+// - disables all the other players XR Controllers
 
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 using Photon.Pun;
 
 public class Li_DisableOtherTrackers : MonoBehaviour
@@ -36,7 +38,11 @@ public class Li_DisableOtherTrackers : MonoBehaviour
                         poseDriver.enabled = false;
                     }
 
-                    //the code to disable tracking of the XR Controllers
+                    //disable the xr controllers (inside of the hands)
+                    foreach (ActionBasedController controller in obj.GetComponentsInChildren<ActionBasedController>())
+                    {
+                        controller.enabled = false;
+                    }
                 }
             }
         }

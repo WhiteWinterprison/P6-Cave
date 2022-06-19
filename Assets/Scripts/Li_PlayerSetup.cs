adf62beb5ccbfbf702a099e515a96ae8d92de6ba
liSiGN <Lilliana11200orlanda>
2022-06-19 20:19:49 +0200

    Fix No Ability to Build WiP
     - from else if to if to remove errors

--- Assets/Scripts/Li_PlayerSetup.cs
+++ Assets/Scripts/Li_PlayerSetup.cs
@@ -83,7 +83,7 @@ public class Li_PlayerSetup : MonoBehaviour
         {
             playerSetup = 3; //its the VR setup
         }*/
-        else if (Display.displays.Length >= displayCount - 1) //if there is a HMD present but there are also 6 displays
+        if (Display.displays.Length >= displayCount - 1) //if there is a HMD present but there are also 6 displays
         {
             playerSetup = 2; //its the CAVE setup
         }
